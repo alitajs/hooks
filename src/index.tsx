3de16428a4e32dcdfa68594ce32786b086a87890
wangxingkang <wangxingkang@sensoro.com>
2019-10-21 12:30:04 +0800

    feat: 目录调整

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { default as useModal } from './useModal';
 export { default as useToggle } from './useToggle';
 export { default as useWindowSize } from './useWindowSize';
-export { default as useFullscreen } from './useFullscreen';
+export { default as useFullScreen } from './useFullScreen';
